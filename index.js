c54b03971780ce7168214c6f4c178bbb4cf7f305
Aniket Kariya <aniket.kariya@gmail.com>
2021-04-25 14:38:19 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname + "/build")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/save-image", (req, res) => {
+    const image = req.body.image
+    
+    fs.readFile('images.json', function (err, data) {
+        if(err) {
+            console.log(err)
+            res.status(500)
+        }
+        
+        var json = JSON.parse(data)
+        json.push({id: json.length, image})
+    
+        fs.writeFile("images.json", JSON.stringify(json), (err, result) => {
+            if(err) console.log(err);
+        })
+    })
+
+    res.status(200);
+})
+
+app.get("/get-images", (req, res) => {
+    res.json(JSON.parse(fs.readFileSync("./images.json")))
+})
+
+app.listen(4000, () => {
+    console.log("up and running on port 4000");
+})
